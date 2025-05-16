e3f706dc60b71621ce1ff76c3cd87d13c92f5c4d
Pedro Moreira(uie54339) <pedro.moreira@conti-engineering.com>
2025-05-16 14:12:21 +0100

    fix.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Anamnese from "./pages/Anamnese";
-import WebPageLovable from "./pages/Lovable/Index";
+import WebPage from "./pages/Lovable/Index";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<WebPageLovable />} />
+      <Route path="/" element={<WebPage />} />
       <Route path="/anamnese" element={<Anamnese />} />
     </Routes>
   );
